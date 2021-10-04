2d0e7395ed74f6dfd6409ead5ea188651a22470b
Arthur Wallace <arthur.wallace.silva@gmail.com>
2021-10-04 15:15:02 -0300

    inserir key para todas as msgs

--- FrontEnd/chat.frontend/src/components/MessageContainer.js
+++ FrontEnd/chat.frontend/src/components/MessageContainer.js
@@ -1,6 +1,5 @@
 var sender = false, admin = false;
 const MessageContainer = ({messages, user}) => {
-    {console.log(messages)}
     return <div className='message-container'>
         {messages.map((m, index) => 
             <div key={index} className='user-message row'>
@@ -8,17 +7,17 @@ const MessageContainer = ({messages, user}) => {
                 {admin = m.userId === 0}
                 {admin 
                 ? [m.message.includes('saiu')
-                    ? <div><span className='message message-admin user-left'>{m.message}</span></div>
-                    : <div><span className='message message-admin'>{m.message}</span></div>]
+                    ? <div key={index}><span className='message message-admin user-left'>{m.message}</span></div>
+                    : <div key={index}><span className='message message-admin'>{m.message}</span></div>]
                 : [sender
-                    ?<div className='text-end'><span className='message message-sent'>{m.message}</span></div>
-                    :<div className='text-start'><span className='message message-received'>{m.message}</span></div>]}
+                    ?<div key={index} className='text-end'><span className='message message-sent'>{m.message}</span></div>
+                    :<div key={index} className='text-start'><span className='message message-received'>{m.message}</span></div>]}
                 
                 {admin 
                 ? ''
                 : [sender
-                    ?<div className='from-user from-user-sent'>{m.user}</div>
-                    :<div className='from-user from-user-received'>{m.user}</div>]}
+                    ?<div key={index} className='from-user from-user-sent'>{m.user}</div>
+                    :<div key={index} className='from-user from-user-received'>{m.user}</div>]}
             </div>
         )}
     </div>
